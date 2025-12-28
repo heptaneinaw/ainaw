93c86b7423bb7ae046c5be1ef941d469b990c7e5
Dev Heptane <dev@heptane.de>
2025-12-28 23:17:56 +0100

    Use base: '/' for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/ainaw/', // Correct base path for your GitHub Pages URL
+  base: '/', // Correct base path for your GitHub Pages URL
   server: {
     host: "::",
     port: 8080,
